2249fc3c1fb851108c9c39778799270b7ae59068
Skander Chouba <skander.chouba@gmail.com>
2020-05-19 18:21:55 +0100

    Complete event-detail component PT.1

--- src/app/events/event-details/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details/event-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../shared/event.service';
 
 @Component({
   selector: 'app-event-details',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-details.component.css']
 })
 export class EventDetailsComponent implements OnInit {
-
-  constructor() { }
+  event: any;
+  constructor(private eventService: EventService) { }
 
   ngOnInit(): void {
+    this.event = this.eventService.getEvent(1)
   }
 
 }
