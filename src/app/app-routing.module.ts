6f4bc4588b88dceff97d677f369d0c37e80afce8
Skander Chouba <skander.chouba@gmail.com>
2020-05-19 23:33:30 +0100

    Not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { EventsListComponent } from './events/events-list/events-list.component';
 import { EventDetailsComponent } from './events/event-details/event-details/event-details.component';
 import { CreateEventComponent } from './events/create-event/create-event.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'events', component: EventsListComponent },
   { path: 'events/new', component: CreateEventComponent },
   { path: 'events/:id', component: EventDetailsComponent },
+  { path: '404', component: NotFoundComponent },
   { path: '', pathMatch: 'full', redirectTo: '/events' }
 ]
 
