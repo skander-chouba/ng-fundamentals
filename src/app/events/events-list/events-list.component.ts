f68dd07b485b0406cb05a6e6a53d68270077b40f
Skander Chouba <skander.chouba@gmail.com>
2020-05-19 17:58:07 +0100

    Making Toastr service

--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
 import { toBase64String } from '@angular/compiler/src/output/source_map';
+import { ToastrService } from 'src/app/common/toastr.service';
 
-declare let toastr;
 @Component({
   selector: 'app-events-list',
   templateUrl: './events-list.component.html',
@@ -11,13 +11,13 @@ declare let toastr;
 export class EventsListComponent implements OnInit {
 
   events: any[];
-  constructor(private eventService: EventService) { }
+  constructor(private eventService: EventService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.events = this.eventService.getEvents();
   }
 
   handleThumbnailClick(name: string){
-    toastr.success(name);
+    this.toastr.success(name);
   }
 }
