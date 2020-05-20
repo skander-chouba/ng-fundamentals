c23acf0f6b999fef76a1b5764fcd3183c38f85a4
Skander Chouba <skander.chouba@gmail.com>
2020-05-20 02:17:43 +0100

    filling the profile component and add a link to it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,11 @@ const routes: Routes = [
   { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivatorService] },
   { path: '404', component: NotFoundComponent },
-  { path: '', pathMatch: 'full', redirectTo: '/events' }
+  { path: '', pathMatch: 'full', redirectTo: '/events' },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+  },
 ]
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-import { ProfileComponent } from '../profile/profile.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
