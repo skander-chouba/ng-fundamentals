15bf4bc7389cd71057361dd86acb7ed8e12665fb
Skander Chouba <skander.chouba@gmail.com>
2020-05-21 10:51:17 +0100

    Add the IEvent Model

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { IEvent } from '../shared';
 
 @Component({
   selector: 'app-event-thumbnail',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 })
 export class EventThumbnailComponent implements OnInit {
 
-  @Input() event: any;
+  @Input() event: IEvent;
   @Output() eventClick = new EventEmitter();
   constructor() { }
 
--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -3,6 +3,7 @@ import { EventService } from '../shared/event.service';
 import { toBase64String } from '@angular/compiler/src/output/source_map';
 import { ToastrService } from 'src/app/common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
+import { IEvent } from '../shared';
 
 @Component({
   selector: 'app-events-list',
@@ -11,7 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EventsListComponent implements OnInit {
 
-  events: any;
+  events: IEvent[];
   constructor(private eventService: EventService, private toastr: ToastrService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
