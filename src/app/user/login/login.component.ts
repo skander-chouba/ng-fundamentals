cd98dba0a610aa69a7dd79d0cd0a2a4b4b312d84
Skander Chouba <skander.chouba@gmail.com>
2020-05-22 16:42:12 +0100

    logging mail and password

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  userName = '';
+  password = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  login(formValue){
+    console.log(formValue);
+  }
 }
