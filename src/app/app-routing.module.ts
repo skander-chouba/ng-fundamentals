f63b439e4c83aedcda3f63ca11f830cd1fc90435
Skander Chouba <skander.chouba@gmail.com>
2020-05-26 00:03:24 +0100

    adding session form and using built in validators

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,12 +9,14 @@ import{
   EventRouteActivatorService,
   EventsListResolverService
 } from './events/index'
+import { CreateSessionComponent } from './events/event-details/create-session/create-session.component';
 
 
 const routes: Routes = [
   { path: 'events', component: EventsListComponent, resolve: {events: EventsListResolverService} },
   { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivatorService] },
+  { path: 'events/session/new', component: CreateSessionComponent },
   { path: '404', component: NotFoundComponent },
   { path: '', pathMatch: 'full', redirectTo: '/events' },
   {
--- src/app/events/event-details/create-session/create-session.component.ts
+++ src/app/events/event-details/create-session/create-session.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { ISession } from '../../shared';
 
 @Component({
   selector: 'app-create-session',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateSessionComponent implements OnInit {
 
+  newSessionForm: FormGroup;
+  name: FormControl;
+  presenter: FormControl;
+  duration: FormControl;
+  level: FormControl;
+  abstract: FormControl;
   constructor() { }
 
   ngOnInit(): void {
+    this.name = new FormControl('', Validators.required);
+    this.presenter = new FormControl('', Validators.required);
+    this.duration = new FormControl('', Validators.required);
+    this.level = new FormControl('', Validators.required);
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)]);
+
+    this.newSessionForm = new FormGroup({
+      name: this.name,
+      presenter: this.presenter,
+      duration: this.duration,
+      level: this.level,
+      abstract: this.abstract
+    });
+  }
+
+  saveSession(formValues): void{
+
+      let session: ISession = {
+        id: undefined,
+        name: formValues.name,
+        presenter: formValues.presenter,
+        duration: +formValues.duration,
+        level: formValues.level,
+        abstract: formValues.abstract,
+        voters: []
+      }
+      console.log(session);
   }
 
 }
