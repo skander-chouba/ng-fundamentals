1bc510c1fe79fa0d88fbcdb8d70a95e71d410f84
Skander Chouba <skander.chouba@gmail.com>
2020-05-29 08:57:32 +0100

    Add sorting functionality

--- src/app/events/event-details/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details/event-details.component.ts
@@ -12,6 +12,7 @@ export class EventDetailsComponent implements OnInit {
   event: IEvent;
   addMode: boolean;
   filterBy = 'all';
+  sortBy = 'votes';
   constructor(private eventService: EventService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
--- src/app/events/event-details/session-list/session-list.component.ts
+++ src/app/events/event-details/session-list/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { ISession } from '../../shared';
 import { faFire } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,14 +8,40 @@ import { faFire } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './session-list.component.html',
   styleUrls: ['./session-list.component.css']
 })
-export class SessionListComponent implements OnInit {
+export class SessionListComponent implements OnChanges {
 
   @Input() sessions: ISession[];
   fireIcon = faFire;
-
+  visibleSessions: ISession[] = [];
+  @Input() filterBy: string;
+  @Input() sortBy: string;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+      this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc);
+    }
   }
 
+  filterSessions(filterBy: string): void{
+    if (filterBy === 'all') {
+      this.visibleSessions = this.sessions.slice(0);
+    }
+    else{
+      this.visibleSessions = this.sessions.filter(x =>{
+        return x.level.toLowerCase() === filterBy
+      } );
+    }
+  }
+}
+
+function sortByNameAsc(s1:ISession, s2: ISession) {
+  if (s1.name > s2.name) return 1;
+  else if(s1.name === s2.name)  return 0;
+  else return -1;
+}
+
+function sortByVotesDesc(s1:ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length;
 }
